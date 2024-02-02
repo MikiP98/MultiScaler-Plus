905a5dba9f10a86d7d3b87d1fb0cffc1a56d0057
MikiP98 <pokora.mikolaj@gmail.com>
2024-02-02 11:16:40 +0100

    Created scaler API

---
+++ scaler.py
@@ -0,0 +1,19 @@
+from PIL import Image
+
+def scale_image(algorithm, pil_image:Image, factor):
+    pass
+
+def scale_image(algorithm, pil_image:Image, output_width, output_height):
+    pass
+
+def scale_image(algorithm, pixels:[[int]], factor):
+    pass
+
+def scale_image(algorithm, pixels:[[int]], output_width, output_height):
+    pass
+
+def scale_image(algorithm, pixels:[int], width, height, factor):
+    pass
+
+def scale_image(algorithm, pixels:[int], input_width, input_height, output_width, output_height):
+    pass
