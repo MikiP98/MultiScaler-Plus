61cc62cfe0ba95f65f610c121ccdd538ccfbaf4f
MikiP98 <pokora.mikolaj@gmail.com>
2024-02-07 18:57:21 +0100

    Small fixes, changed if else stack to match case

--- scaler.py
+++ scaler.py
@@ -1,53 +1,83 @@
 # coding=utf-8
-from PIL import Image
+import numpy as np
+import torch
+import xbrz  # See xBRZ scaling on Jira
+
 from enum import IntEnum
-import xbrz # See xBRZ scaling on Jira
+from PIL import Image
+from RealESRGAN import RealESRGAN
 
 
+# Enum with all available algorithms
+# Ordered alphabetically with number indicating the quality from 0 (lowest) up
 class Algorithms(IntEnum):
+    BICUBIC = 3
+    BILINEAR = 2
+    LANCZOS = 4
     NEAREST_NEIGHBOR = 0
-    xBRZ = 1
-    RealESRGAN = 2
-    BILINEAR = 3
-    BICUBIC = 4
-    LANCZOS = 5
+    RealESRGAN = 6
+    xBRZ = 5
 
 
-# Current main function
-def scale_image(algorithm, pil_image:Image, factor) -> Image:
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+# Main function for Python for existing libs
+def scale_image(algorithm, pil_image:Image, factor, main_checked=False) -> Image:
     pil_image = pil_image.convert('RGBA')
     width, height = pil_image.size
+    output_width, output_height = width * factor, height * factor
 
-    if algorithm == Algorithms.BICUBIC:
-        return pil_image.resize((width*factor, height*factor), Image.BICUBIC)
-    elif algorithm == Algorithms.BILINEAR:
-        return pil_image.resize((width*factor, height*factor), Image.BILINEAR)
-    elif algorithm == Algorithms.NEAREST_NEIGHBOR:
-        return pil_image.resize((width*factor, height*factor), Image.NEAREST)
-    elif algorithm == Algorithms.LANCZOS:
-        return pil_image.resize((width*factor, height*factor), Image.LANCZOS)
-    elif algorithm == Algorithms.xBRZ:
-        if factor > 6:
-            raise ValueError("Max factor for xbrz=6")
-        return xbrz.scale_pillow(pil_image, factor)
-    elif algorithm == Algorithms.RealESRGAN:
-        raise NotImplementedError("Not implemented yet")
-
+    match algorithm:
+        case Algorithms.NEAREST_NEIGHBOR:
+            return pil_image.resize((output_width, output_height), Image.NEAREST)
+        case Algorithms.BILINEAR:
+            return pil_image.resize((output_width, output_height), Image.BILINEAR)
+        case Algorithms.BICUBIC:
+            return pil_image.resize((output_width, output_height), Image.BICUBIC)
+        case Algorithms.LANCZOS:
+            return pil_image.resize((output_width, output_height), Image.LANCZOS)
+        case Algorithms.xBRZ:
+            if factor > 6:
+                raise ValueError("Max factor for xbrz=6")
+            return xbrz.scale_pillow(pil_image, factor)
+        case Algorithms.RealESRGAN:
+            model = RealESRGAN(device, scale=4)
+            model.load_weights('weights/RealESRGAN_x4.pth', download=True)
+            image = pil_image.convert('RGB')
+            return model.predict(image)
+        case _:
+            if main_checked:
+                raise NotImplementedError("Not implemented yet")
+            else:
+                width, height = pil_image.size
+                pixels = [[[int]]]
+                for y in range(height):
+                    for x in range(width):
+                        pixels[y][x] = pil_image.getpixel((x, y))
+                return scale_image_data(algorithm, pixels, width, height, factor, True)
 
 # def scale_image(algorithm, pil_image:Image, output_width, output_height):
 #     pass
 
-# def scale_image(algorithm, pixels:[[int]], factor):
-#     if algorithm == Algorithms.NEAREST_NEIGHBOR:
-#         return nearest_neighbor.scale_image(pil_image, factor)
-#     else:
-#         raise NotImplementedError("Algorithm not implemented yet")
+# Main function for C++ lib
+def scale_image_data(algorithm, pixels:[[[int]]], factor, main_checked=False):
+    match algorithm:
+        case _:
+            if main_checked:
+                raise NotImplementedError("Not implemented yet")
+            else:
+                image = Image.new("RGBA", (len(pixels[0]) * factor, len(pixels) * factor))
+                for y in range(len(pixels)):
+                    for x in range(len(pixels[0])):
+                        image.putpixel((x * factor, y * factor), pixels[y][x])
+                return scale_image(algorithm, image, factor, True)
 
-# def scale_image(algorithm, pixels:[[int]], output_width, output_height):
+# def scale_image(algorithm, pixels:[[[int]]], output_width, output_height):
 #     pass
-
-# def scale_image(algorithm, pixels:[int], width, height, factor):
+#
+# def scale_image(algorithm, pixels:[[int]], width, height, factor):
 #     pass
-
-# def scale_image(algorithm, pixels:[int], input_width, input_height, output_width, output_height):
+#
+# def scale_image(algorithm, pixels:[[int]], input_width, input_height, output_width, output_height):
 #     pass
--- standalone.py
+++ standalone.py
@@ -1,3 +1,4 @@
+import os
 import scaler
 
 from PIL import Image
@@ -5,3 +6,9 @@ from PIL import Image
 if __name__ == '__main__':
     path = "input/blast_furnace_front.png"
     image = scaler.scale_image(scaler.Algorithms.xBRZ, Image.open(path), 4)
+
+    # Create output directory if it doesn't exist
+    if not os.path.exists("output"):
+        os.makedirs("output")
+
+    image.save("output/blast_furnace_front.png")
