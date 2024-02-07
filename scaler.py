28d6e3ed3d7ec015ef69e845342fb00a643ba8c0
MikiP98 <pokora.mikolaj@gmail.com>
2024-02-07 09:59:40 +0100

    Stuff

--- scaler.py
+++ scaler.py
@@ -1,27 +1,28 @@
 # coding=utf-8
-import xbrz_py__1_0_2.xbrz
+import scalers.nearest_neighbor
 
 from PIL import Image
 from enum import IntEnum
 
 
 class Algorithms(IntEnum):
+    NEAREST_NEIGHBOR = 0
     xBRZ = 1
     RealESRGAN = 2
 
 
 # Current main function
 def scale_image(algorithm, pil_image:Image, factor):
-    if algorithm == Algorithms.xBRZ:
-        return xbrz_py__1_0_2.xbrz.scale(pil_image, factor, pil_image.width, pil_image.height, xbrz_py__1_0_2.xbrz.ColorFormat.RGBA)
-    else:
-        raise NotImplementedError("Algorithm not implemented yet")
+    pass
 
 def scale_image(algorithm, pil_image:Image, output_width, output_height):
     pass
 
 def scale_image(algorithm, pixels:[[int]], factor):
-    pass
+    if algorithm == Algorithms.NEAREST_NEIGHBOR:
+        return nearest_neighbor.scale_image(pil_image, factor)
+    else:
+        raise NotImplementedError("Algorithm not implemented yet")
 
 def scale_image(algorithm, pixels:[[int]], output_width, output_height):
     pass
