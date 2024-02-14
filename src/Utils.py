11633732b84ebe327af23bd7015fb2340e5fbbdd
Piotr Przetacki <pprzetacki@tm1.edu.pl>
2024-02-14 10:29:55 +0100

    Add /scale endpoint

--- src/Utils.py
+++ src/Utils.py
@@ -1,1 +1,30 @@
-# coding=utf-8
+from email.policy import default
+import io
+from PIL import Image
+from scaler import Algorithms
+
+def image_to_byte_array(image: Image) -> bytes:
+  # BytesIO is a file-like buffer stored in memory
+  imgByteArr = io.BytesIO()
+  # image.save expects a file-like as a argument
+  image.save(imgByteArr, format='PNG')
+  # Turn the BytesIO object back into a bytes object
+  imgByteArr = imgByteArr.getvalue()
+  return imgByteArr
+
+def string_to_scaling_algorithm(string: str) -> Algorithms:
+    match string:
+        case 'nearest-neighbour':
+            return Algorithms.NEAREST_NEIGHBOR
+        case 'bilinear':
+            return Algorithms.BILINEAR
+        case 'bicubic':
+            return Algorithms.BICUBIC
+        case 'lanczos':
+            return Algorithms.LANCZOS
+        case 'xbrz':
+            return Algorithms.xBRZ
+        case 'esrgan':
+            return Algorithms.RealESRGAN
+        case _:
+            raise ValueError("Algorithm not found")
--- src/server.py
+++ src/server.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, HTTPException, UploadFile
+from fastapi.responses import Response
+from PIL import Image 
+from Utils import image_to_byte_array, string_to_scaling_algorithm
+from scaler import scale_image
 
 app = FastAPI()
 
@@ -7,6 +10,20 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/test")
-async def main():
-    return FileResponse("input/example_shell_40px.png")
+@app.get(
+    "/scale",
+    responses = {
+        200: {
+            "content": {"image/png": {}}
+        }
+    },
+    response_class=Response
+)
+async def main(content: UploadFile, algorithm: str = 'bicubic', factor: float = 2):
+    img = Image.open(content.file)
+    try:
+        scaling_algorithm = string_to_scaling_algorithm(algorithm.lower())
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    scaled_image = scale_image(scaling_algorithm, img, factor)
+    return Response(content=image_to_byte_array(scaled_image), media_type="image/png")
