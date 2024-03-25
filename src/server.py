9c64c804f05f36356e9392f510df771e5b36f415
Mikolaj Pokora <pokora.mikolaj@gmail.com>
2024-03-25 09:02:15 +0100

    fixed API?

--- src/server.py
+++ src/server.py
@@ -34,9 +34,17 @@ def read_root():
 )
 def main(content: UploadFile, algorithm: str = 'bicubic', factor: float = 2):
     img = Image.open(content.file)
+
     try:
         scaling_algorithm = string_to_algorithm(algorithm.lower())
+        if not scaling_algorithm:
+            raise ValueError(f"\"{algorithm}\" does not exist!")
+
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
-    scaled_image = scale_image_batch(scaling_algorithm, img, [factor])
+
+    scaled_image = scale_image_batch(scaling_algorithm, img, [factor]).get()
+    if not scaled_image:
+        scaled_image = Image.open("./web_temp_output/image.png")
+
     return Response(content=image_to_byte_array(scaled_image), media_type="image/png")
