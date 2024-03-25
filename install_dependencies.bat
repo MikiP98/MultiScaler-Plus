c82a4d9ce0eedc33c5c92a434affc2feb4521422
Mikolaj Pokora <pokora.mikolaj@gmail.com>
2024-03-25 08:34:29 +0100

    removed unused stuff

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,5 +1,5 @@
 @cd src
 git submodule update --init --recursive
-pip install --no-dependencies transformers==4.31.0
+@REM pip install --no-dependencies transformers==4.31.0
 pip install -r requirements.txt
 @PAUSE
