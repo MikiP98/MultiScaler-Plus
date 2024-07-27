3a23d323408b0ecebfb3fe2abde852cef841445a
MikiP98 <pokora.mikolaj@gmail.com>
2024-07-27 23:51:35 +0200

    Brought back/finished some saver functionality from standalone

--- src/saver.py
+++ src/saver.py
@@ -1,6 +1,8 @@
+# coding=utf-8
 # File for saving images
 
 import numpy as np
+import os
 import PIL.Image
 import qoi
 import utils
@@ -24,7 +26,11 @@ class SimpleConfig(TypedDict):
 
 class AdvancedConfig(TypedDict):
     simple_config: SimpleConfig
-    file_additives: dict[str, bool]
+
+    add_factor_to_name: bool
+    sort_by_factor: bool
+
+    factors: list[float]
 
 
 def generate_file_path(base_path, compression, extension, add_compression_to_name):
@@ -53,8 +59,10 @@ def save_image_with_png(image: PIL.Image, path: str, config: SimpleConfig):
         else:
             img_byte_arr = utils.apply_lossless_compression_png(image)
             file_path = generate_file_path(path, compression, "png", config['add_compression_to_name'])
-            with open(file_path, 'wb') as f:
+            with open(file_path, 'wb+') as f:
                 f.write(img_byte_arr)
+
+
 #     if "PNG" in config['formats']:
 #         for compression in config['compressions']:
 #             if not compression['lossless']:  # if lossy
@@ -88,6 +96,8 @@ def save_image_with_qoi(image: PIL.Image, path: str, config: SimpleConfig):
         file_path = generate_file_path(path, compression, "qoi", config['add_compression_to_name'])
         with open(file_path, 'wb') as f:
             f.write(image_bytes)
+
+
 #     if "QOI" in config['formats']:
 #         for compression in config['compressions']:
 #             if not compression['lossless']:
@@ -134,6 +144,8 @@ def save_image_with_webp(image: PIL.Image, path: str, config: SimpleConfig):
         else:
             if not compression['additional_lossless']:
                 image.save(file_path, quality=compression['quality'], method=6)
+
+
 #     if "WEBP" in config['formats']:
 #         for compression in config['compressions']:
 #             if compression['lossless']:
@@ -157,6 +169,8 @@ def save_image_with_avif(image: PIL.Image, path: str, config: SimpleConfig):
             )
         else:
             image.save(file_path, quality=compression['quality'], speed=0, range="full")
+
+
 #     if "AVIF" in config['formats']:
 #         for compression in config['compressions']:
 #             if compression['lossless']:
@@ -182,11 +196,34 @@ format_savers = {
 def save_image(image: PIL.Image, path: str, config: SimpleConfig) -> None:
     print(path)
 
+    if not os.path.exists(os.path.dirname(path)):
+        os.makedirs(os.path.dirname(path))
+
     for format_name in config['formats']:
+        # print(colored(f"Saving {format_name}...", 'light_cyan'))
         format_savers[format_name](image, path, config)
+        # print(colored(f"{format_name} Fully Saved!", 'light_green'))
 
     print(colored(f"{path} Fully Saved!", 'light_green'))
 
 
-def save_image_pre_processor(image: PIL.Image, output_path: str, file_name: str, config: AdvancedConfig) -> None:
-    save_image(image, f"{output_path}/{file_name}", config['simple_config'])
+def save_image_pre_processor(image: utils.ImageDict, output_path: str, file_name: str, config: AdvancedConfig) -> None:
+    # if config['add_factor_to_name']:
+    #     for factor in config['factors']:
+    #         save_image(image, f"{output_path}/{file_name}_{factor}", config['simple_config'])
+
+    if len(image["images"][0]) > 1:
+        print("Stacked and animated images are not supported yet")
+        return
+
+    if config['sort_by_factor']:
+        for filtered_image, factor in zip(image["images"], config['factors']):
+            save_image(filtered_image[0], os.path.join(output_path, str(factor), file_name), config['simple_config'])
+
+    elif config['add_factor_to_name']:
+        for filtered_image, factor in zip(image["images"], config['factors']):
+            save_image(filtered_image[0], os.path.join(output_path, f"{file_name}_{factor}"), config['simple_config'])
+
+    else:
+        for filtered_image in image["images"]:
+            save_image(filtered_image[0], os.path.join(output_path, file_name), config['simple_config'])
