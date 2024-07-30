c3c0e061014d35cc51a21a1f23f4c4866208e38a
MikiP98 <pokora.mikolaj@gmail.com>
2024-07-30 18:13:33 +0200

    Created default loader config and started adding filter config

--- src/config.py
+++ src/config.py
@@ -1,1 +1,28 @@
 # coding=utf-8
+
+
+default_configs = {
+    "loader": {
+        'clear_output_dir': True,
+        'copy_mcmeta': True
+    },
+    "filter": {
+        "simple_config": {
+            "formats": ["PNG"],
+            "compressions": [
+                {
+                    "additional_lossless": True,
+                    "lossless": True
+                }
+            ],
+            "add_compression_to_name": False
+        },
+
+        "add_factor_to_name": False,
+        "sort_by_factor": True
+    },
+}
+
+
+def get_loader_config() -> dict:
+    return default_configs["loader"]
