e3dda79dbbffcbd0095a45c135a3278f9feea642
MikiP98 <pokora.mikolaj@gmail.com>
2024-07-31 15:45:45 +0200

    Finished saver plugin_example.py

--- src/saving/plugin_example.py
+++ src/saving/plugin_example.py
@@ -1,2 +1,11 @@
 # coding=utf-8
-# TODO: Implement this file
+import PIL.Image
+
+from saving.saver import Compression, format_savers
+
+
+def plugin_save(image: PIL.Image, path: str, compression: Compression):
+    ...
+
+
+format_savers['plugin_file_format'] = plugin_save
