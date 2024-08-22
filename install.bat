2f483399a2bd20b2e3bfb7be4173c405357cbf3a
MikiP98 <pokora.mikolaj@gmail.com>
2024-08-22 11:58:46 +0200

    Fixed install scripts in install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass .\content\install_scripts\install_vs_buildtools.ps1
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass .\content\install_scripts\InstallImDisk.ps1
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass .\content\resources\install_scripts\install_vs_buildtools.ps1
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass .\content\resources\install_scripts\InstallImDisk.ps1
 
 @cd content\src
 
