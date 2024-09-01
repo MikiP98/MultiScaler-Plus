6b0c5df35bbd87a465bb3e5e0b819a19bc0d7d97
MikiP98 <pokora.mikolaj@gmail.com>
2024-09-01 13:09:21 +0200

    Added forced palette as a PNG lossy format

--- content/src/saving/file_formats/png.py
+++ content/src/saving/file_formats/png.py
@@ -7,24 +7,39 @@ from termcolor import colored
 
 
 def save(image: PIL.Image.Image, path: str, compression: Compression, sort_by_file_extension: bool) -> None:
-    if not compression['lossless']:  # if lossy
-        print(colored(
-            "WARN: You CAN use lossy compression with PNG format, but this app does not support it :(\n"
-            "SKIPPING", 'yellow'))
-        return
-        # TODO: make a force palette as a lossy PNG compression for now
-
     path = utils.sort_by_file_extension(path, sort_by_file_extension, "PNG")
 
     file_path = path + "png"
 
-    # If lossless
-    if not compression['additional_lossless']:
+    if not compression['lossless']:  # if lossy
+        print(colored(
+            "WARN: You CAN use lossy compression with PNG format, but this app does not support it :(\n"
+            "\tMaking a forced palette PNG", 'yellow'))
+        if not compression["additional_lossless"]:
+            image = image.convert('P', palette=PIL.Image.ADAPTIVE, colors=255)
+        else:
+            unique_colors_number = utils.count_unique_colors_python_break_batched(image)
+
+            colors = 2  # benchmarked
+            if unique_colors_number > 16:
+                colors = 256
+            elif unique_colors_number > 4:
+                colors = 16
+            elif unique_colors_number > 2:
+                colors = 4
+
+            image = image.convert('P', palette=PIL.Image.ADAPTIVE, colors=colors)
+
         image.save(file_path, optimize=True)
-    else:  # if additional lossless
-        img_byte_arr = apply_additional_lossless_compression(image)
-        with open(file_path, 'wb+') as f:
-            f.write(img_byte_arr)
+
+    else:
+        # If lossless
+        if not compression['additional_lossless']:
+            image.save(file_path, optimize=True)
+        else:  # if additional lossless
+            img_byte_arr = apply_additional_lossless_compression(image)
+            with open(file_path, 'wb+') as f:
+                f.write(img_byte_arr)
 
 
 def apply_additional_lossless_compression(image: PIL.Image) -> bytes:
